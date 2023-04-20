8ed7facaff6031e283d95c2f419153801d886a77
GenyaElamkov <GenyaElamkov@gmail.com>
2023-04-20 10:00:04 +0300

    [upd] file main, start

---
+++ src/sms/main.py
@@ -0,0 +1,6 @@
+def main():
+    print('SMS')
+
+
+if __name__ == '__main__':
+    main()
---
+++ start.py
@@ -0,0 +1,9 @@
+from src.sms.main import main
+
+
+def start():
+    main()
+
+
+if __name__ == '__main__':
+    start()
