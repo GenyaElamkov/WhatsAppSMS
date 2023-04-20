b7abc6aa2084229f5ed0a6911068a69bada6ce2a
GenyaElamkov <GenyaElamkov@gmail.com>
2023-04-20 10:35:45 +0300

    [upd] proba

--- src/sms/main.py
+++ src/sms/main.py
@@ -1,6 +1,21 @@
-def main():
-    print('SMS')
+import os
 
+from dotenv import load_dotenv
+from whatsapp_api_client_python import API
+
+load_dotenv()
+
+
+def main():
+    API_TOKEN_INSTANCE = os.getenv('API_TOKEN_INSTANCE')
+    ID_INSTANCE = os.getenv('ID_INSTANCE')
+    NUMBER_PHONE = os.getenv('NUMBER_PHONE')
+
+    greenAPI = API.GreenApi(ID_INSTANCE, API_TOKEN_INSTANCE)
+    text = 'Приветик. Работает скрипт. Ответь если увидела сообщения.'
+    result = greenAPI.sending.sendMessage(NUMBER_PHONE, text)
+
+    print(result)
 
 if __name__ == '__main__':
     main()
